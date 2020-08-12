edf01911e26763a253886546a2a9f0ce75725c42
Scott H Lee <slee@logicfirstllc.com>
2020-08-12 22:15:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,9 @@ def game_hash
  game_hash = {:home => {
               :team_name => "Brooklyn Nets",
               :colors => ["Black", "White"],
-              :players =>[]
+              :players =>[{
+                          :player_name => "Alan Anderson"
+              }]
    
                        },
               
